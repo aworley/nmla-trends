76d14ae2c1fc5b102436a5fdcf729e63f2428c23
Aaron Worley <aaron@pikasoftware.com>
2016-06-10 10:42:00 -0400

    Change HTML header relative URLs to absolute URLs.

--- www/nmtrends/html_header.php
+++ www/nmtrends/html_header.php
@@ -1,15 +1,15 @@
-<script language="javascript" type="text/javascript" src="js/jquery.min.js"></script>
-<script language="javascript" type="text/javascript" src="js/jquery.jqplot.min.js"></script>
-<script type="text/javascript" src="js/plugins/jqplot.canvasTextRenderer.min.js"></script>
-<script type="text/javascript" src="js/plugins/jqplot.canvasAxisLabelRenderer.min.js"></script>
-<script type="text/javascript" src="js/plugins/jqplot.dateAxisRenderer.min.js"></script>
-<link rel="stylesheet" type="text/css" href="js/jquery.jqplot.min.css" />
+<script language="javascript" type="text/javascript" src="<?php echo $base_url; ?>/js/jquery.min.js"></script>
+<script language="javascript" type="text/javascript" src="<?php echo $base_url; ?>/js/jquery.jqplot.min.js"></script>
+<script type="text/javascript" src="<?php echo $base_url; ?>/js/plugins/jqplot.canvasTextRenderer.min.js"></script>
+<script type="text/javascript" src="<?php echo $base_url; ?>/js/plugins/jqplot.canvasAxisLabelRenderer.min.js"></script>
+<script type="text/javascript" src="<?php echo $base_url; ?>/js/plugins/jqplot.dateAxisRenderer.min.js"></script>
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>/js/jquery.jqplot.min.css" />
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
 
     <!-- Bootstrap -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <link href="<?php echo $base_url; ?>/css/bootstrap.min.css" rel="stylesheet">
 
     <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
     <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
@@ -18,4 +18,4 @@
       <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-<link rel="stylesheet" type="text/css" href="css/trends.css">
+<link rel="stylesheet" type="text/css" href="<?php echo $base_url; ?>/css/trends.css">
